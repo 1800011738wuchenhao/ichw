be309fd8a2eb74cbf90ecfa68d78e634fe4548a2
wuchenhao <43374613+1800011738wuchenhao@users.noreply.github.com>
2018-11-06 23:57:40 +0800

    Add files via upload

--- pyassign1/planets.py
+++ pyassign1/planets.py
@@ -1,6 +1,7 @@
 import math
 import turtle
 wn=turtle.Screen()
+turtle.bgcolor('black')
 a=turtle.Turtle()
 b=turtle.Turtle()
 c=turtle.Turtle()
@@ -20,7 +21,7 @@ def planet(name,color):
 planet(a,'blue')
 planet(b,'green')
 planet(c,'red')
-planet(d,'black')
+planet(d,'pink')
 planet(e,'yellow')
 planet(f,'brown')
 
@@ -28,23 +29,23 @@ def origin(t,x):
     t.up()
     t.goto(x,0)
     t.down()
-origin(a,50)
-origin(b,80)
-origin(c,110)
-origin(d,140)
-origin(e,170)
-origin(f,200)
+origin(a,30)
+origin(b,48)
+origin(c,66)
+origin(d,84)
+origin(e,102)
+origin(f,120)
     
 def orbit(t,a,e,o):    
     r=a*(1-e**2)/(1+e*math.cos(math.radians(o)))
-    x=r*math.cos(math.radians(o))+a*e
+    x=r*math.cos(math.radians(o))
     y=r*math.sin(math.radians(o))
     t.goto(x,y)
 
 for o in range(1440):
-    orbit(a,50,0.6,8*o)
-    orbit(b,80,0.6,8*o/2.023857703)
-    orbit(c,110,0.6,8*o/3.263127334)
-    orbit(d,140,0.6,8*o/4.685296149)
-    orbit(e,170,0.6,8*o/6.269290231)
-    orbit(f,200,0.6,o)
+    orbit(a,50,0.4,8*o)
+    orbit(b,80,0.4,8*o/2.023857703)
+    orbit(c,110,0.4,8*o/3.263127334)
+    orbit(d,140,0.4,8*o/4.685296149)
+    orbit(e,170,0.4,8*o/6.269290231)
+    orbit(f,200,0.4,o)
